ac9c698bb5f6911253e4eef3856096bd0672ee84
Asmar Shahid <l211754@lhr.nu.edu.pk>
2026-07-18 12:30:33 +0500

    second

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(),react()],
-  base: "/",
+  base: "/LaRivalite/",
   server: {
     host: true,
     port: 3000
